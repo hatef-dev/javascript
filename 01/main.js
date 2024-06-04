68bc2be171b7a943104dad579056b7f5b3dc1d89
hatef-dev <hatef.dev21@gmail.com>
2024-06-04 21:45:18 +0330

    finish project

--- 01/main.js
+++ 01/main.js
@@ -6,6 +6,7 @@ const message = document.querySelector(".message");
 const againBtn = document.querySelector(".again");
 let score = 20;
 let inputNumber = null;
+let hightScore = 0;
 const randNumber = Math.trunc(Math.random() * 20 + 1);
 
 const eventsHandelr = function () {
@@ -16,19 +17,20 @@ const eventsHandelr = function () {
     message.textContent = "Correct answer!!!";
     showCorrectNumber.textContent = randNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
+    console.log(score)
     showCorrectNumber.style.width = "30rem";
-  } else if (randNumber > inputNumber) {
-    if (score > 0) {
-      message.textContent = "Too Low";
-      score--;
-      showScore.textContent = score;
+    if (score > hightScore) {
+      hightScore = score
+      document.querySelector(".highscore").textContent = hightScore;
+      
     }
-  } else if (randNumber < inputNumber) {
+  } 
+  else if (randNumber !== inputNumber){
     if (score > 0) {
-      message.textContent = "Too High";
-      score--;
-      showScore.textContent = score;
-    }
+        message.textContent = randNumber > inputNumber ? 'Too Low' : 'Too High';
+        score--;
+        showScore.textContent = score;
+      }
   }
 };
 console.log(randNumber);
