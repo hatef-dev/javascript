2a1a2fab24bacec94d4b5979fcfd8f47385ff295
hatef-dev <hatef.dev21@gmail.com>
2024-06-05 12:07:51 +0330

    create modals

---
+++ 02/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenMadals = document.querySelectorAll('.show-modal')
+console.log(btnsOpenMadals)
+
+for (let index = 0; index < btnsOpenMadals.length; index++) {
+    console.log(btnsOpenMadals[index])
+    
+}
