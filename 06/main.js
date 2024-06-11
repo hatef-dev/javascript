9c275669ab45633d2b68a395a596f3ac47e2f1f9
hatef-dev <hatef.dev21@gmail.com>
2024-06-11 20:49:59 +0330

    finish cookie project

--- 06/main.js
+++ 06/main.js
@@ -1,4 +1,14 @@
 import { gsap } from "gsap";
 
-gsap.fromTo(".text", {y:60, opacity: 0}, {y:0, opacity:1, direction:1.5})
-// gsap.registerPlugin(MotionPathPlugin, ScrollToPlugin, TextPlugin);
+const btn = document.querySelector('button')
+const tl = gsap.timeline({defaults: {duration: 0.75}})
+
+tl.fromTo('.cookie-container',{scale:0}, {scale: 1, direction:2, ease: "elastic.out(1,0.5)"} )
+tl.fromTo('.text', {x: 45, opacity: 0}, {x: 0, opacity:1}, "<60%");
+tl.fromTo('.cookie', {x: -45, opacity: 0, rotation: "-45deg"}, {x:  0, opacity: 1, rotation: "0deg"}, "<")
+tl.fromTo('.cookie', {y:0, rotation:"0deg"}, {y:-20,rotation:"-20deg", yoyo: true, repeat: -1})//repeat always loop
+tl.fromTo('#crumbs', {y:0}, {y:-10, yoyo: true, repeat: -1}, "<")//repeat always loop
+tl.fromTo
+btn.addEventListener('click', () => {
+  gsap.to('.cookie-container', {scale:0, opacity:0, direction:2.5, ease: "elastic.in(1,0.5)"})
+})
