184a4f22ff7ae8559c76b7117cbf8bdf63b6b311
hatef-dev <hatef.dev21@gmail.com>
2024-06-11 14:11:15 +0330

    solve the problem twitter and api joke

--- 05/main.js
+++ 05/main.js
@@ -1,14 +1,32 @@
-
-
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const qutoeBtn = document.getElementById("new-quote");
+let data = null;
 async function getData() {
-
   const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,racist,sexist&type=single";
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    data = await response.json();
+    quoteAuthor.textContent = "Hatef Sanati"
+    if (data.joke.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = data.joke;
+    // getNewQuote()
   } catch (error) {
     console.log(error);
   }
 }
+const tweetQuote = function() {
+  const twitterURl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}`
+  window.open(twitterURl, '_blank')
+}
+qutoeBtn.addEventListener('click', getData)
+twitterBtn.addEventListener('click', tweetQuote)
+getData();
+
