db0c98d9d9f2a8be27a8f73299515e4b53512895
hatef-dev <hatef.dev21@gmail.com>
2024-06-11 19:25:35 +0330

    add gsap wit npm

--- 06/main.js
+++ 06/main.js
@@ -1,3 +1,4 @@
 import { gsap } from "gsap";
 
-gsap.to()
+gsap.fromTo(".text", {y:60, opacity: 0}, {y:0, opacity:1, direction:1.5})
+// gsap.registerPlugin(MotionPathPlugin, ScrollToPlugin, TextPlugin);
