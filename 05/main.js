18870e0ba0c8bf557ac26b1b5469d09ed57b3c4e
hatef-dev <hatef.dev21@gmail.com>
2024-06-11 16:50:50 +0330

    add loading

--- 05/main.js
+++ 05/main.js
@@ -3,14 +3,27 @@ const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const qutoeBtn = document.getElementById("new-quote");
-let data = null;
+const loader = document.getElementById('loader')
+
+
+const loading = function() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+const complete = function() {
+  loader.hidden= true ;
+  quoteContainer.hidden = false;
+}
+
 async function getData() {
+  loading();
   const apiUrl =
     "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,racist,sexist&type=single";
   try {
+    loading()
     const response = await fetch(apiUrl);
-    data = await response.json();
-    quoteAuthor.textContent = "Hatef Sanati"
+    let data = await response.json();
+    quoteAuthor.textContent = "Hatef Sanati";
     if (data.joke.length > 120) {
       quoteText.classList.add("long-quote");
     } else {
@@ -21,12 +34,12 @@ async function getData() {
   } catch (error) {
     console.log(error);
   }
+  complete()
 }
-const tweetQuote = function() {
-  const twitterURl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}`
-  window.open(twitterURl, '_blank')
-}
-qutoeBtn.addEventListener('click', getData)
-twitterBtn.addEventListener('click', tweetQuote)
+const tweetQuote = function () {
+  const twitterURl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}`;
+  window.open(twitterURl, "_blank");
+};
+qutoeBtn.addEventListener("click", getData);
+twitterBtn.addEventListener("click", tweetQuote);
 getData();
-
