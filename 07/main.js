20a2b1d72e0d644c5cbb46b0683a6abbbbeb32b4
hatef-dev <hatef.dev21@gmail.com>
2024-06-14 14:57:18 +0330

    finished

--- 07/main.js
+++ 07/main.js
@@ -1,24 +1,33 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { gsap } from "gsap";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.in" } });
 
-setupCounter(document.querySelector('#counter'))
+tl.fromTo(
+  ".hero-img",
+  { scale: 1.3, borderRadius: "0rem" },
+  {
+    scale: 1,
+    borderRadius: "2rem",
+    delay: 0.25,
+    duration: 2.5,
+    ease: "elastic.out(1.5,1)",
+  }
+);
+
+tl.fromTo(".cta1", { x: "100%", opacity: 0 }, { x: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta3", { x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta2", { y: "-100%", opacity: 0 }, { y: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta4", { x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta6", { x: "100%", opacity: 0 }, { x: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta5", { y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<10%");
+tl.fromTo(".cta-btn", { y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<");
+
+const logo = document.querySelector(".logo");
+const letters = logo.textContent.split("");
+
+logo.textContent = ""
+for(const index of letters){
+  logo.innerHTML+= '<span class="letter">'+ index + '</span>'
+}
+gsap.set('.letter', {display: 'inline-block'})
+gsap.fromTo('.letter', {y: "100%",},{y: 0, stagger:0.1, delay: 1, duration:0.75, ease: "back.inOut"})
